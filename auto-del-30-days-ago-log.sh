f74dffff3b7ab72a35450873482313aa3efe0c43
Yanchao <327889908@qq.com>
2018-04-15 00:05:58 +0800

    Update auto-del-30-days-ago-log.sh

--- auto-del-30-days-ago-log.sh
+++ auto-del-30-days-ago-log.sh
@@ -6,6 +6,10 @@
 
 dir= /opt/soft/log/   #删除指定目录下日志文件
 find $dir -mtime +30 -name "*.log" -exec rm -rf {} \;
+#find /mnt/sdb/backups -mtime +15 -name "*gz*" -exec rm -rf {} \;
+#删除 /mnt/sdb/backups文件夹下 15天前的 文件名包含 "gz" 的文件
+#如果要删除当前目录包括子目录下面的文件名包含"gz"﻿的文件，用
+#find /mnt/sdb/backups/ -mtime +15 -type f -iname "*gz*" -exec rm -rf {} \;﻿
 
 #crontab -e
 #将auto-del-30-days-ago-log.sh执行脚本加入到系统计划任务，到点自动执行
