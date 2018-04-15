6928115f1fb1bb83838ce232c6ed8d8f9aa91ef2
Yanchao <327889908@qq.com>
2018-04-15 11:01:16 +0800

    Update test_insert_data_redis.sh

--- test_insert_data_redis.sh
+++ test_insert_data_redis.sh
@@ -1,1 +1,15 @@
 
+#/bin/sh
+
+dir= /data/redis/bin7K/
+
+n=100000000
+for ((i=n;i>=1;i--))
+do
+    echo $i
+    sh $dir/redis-cli -p 7000 dbsize | xargs redis-cli -p 7000 set name$i xxxx       
+done
+~     
+
+
+
