08c993d9945a517248a91cb71a5151ea52566211
Yanchao <327889908@qq.com>
2018-04-15 11:14:30 +0800

    Create monitor_mysql_status_4.sh
    monitor_mysql_status via client

---
+++ monitor_mysql_status_4.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+#author: eyanchao
+ 
+mysql -uroot -p123456 -e "select version();" &>/dev/null
+if [ $? -ne 0 ]
+then
+ /etc/init.d/mysqld start
+else
+ echo "MySQL is running"
+fi
+
+
+
